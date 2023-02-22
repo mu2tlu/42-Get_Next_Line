123aed42fe57ff93fb748c1892c8f3f43ee4579e
4mu2 <115688354+4mu2@users.noreply.github.com>
2023-02-22 23:58:10 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,12 @@
-#include "get_next_line.h"
 int main()
 {
     int fd;
-
-    fd = open("foo.txt",O_RDONLY);
-    printf("%s",get_next_line(fd));
-    printf("%s",get_next_line(fd));
+    int i = 0;
+    fd = open("4mu2.txt",O_RDONLY);
+    while(i < 10)
+    {
+        printf("%s",get_next_line(fd));
+		i++;
+    }
+	return 0;
 }
